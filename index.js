8a5ce30609a13099bcc25461841540459f31a640
David Pineau <david.pineau@scality.com>
2015-12-15 14:40:32 +0100

    dev(Error Codes): Import Error codes
    They will be used as common resource for both client and server, and thus shall
    be included in the client code. As such, we add it to the exports, so tht the
    server may use them later on.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('./es6-compat.js');
 
 modules.export = {
-    default: require('lib/IAMClient.js').default;
+    default: require('lib/IAMClient.js').default,
+    errorCode: require('lib/ErrorCodes.js').errorCode,
 };
