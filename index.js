e974e98af1159a35cb104a246629fe731578d58e
David Pineau <david.pineau@scality.com>
2015-12-15 18:55:48 +0100

    dev(index): Fix require paths

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('./es6-compat.js');
 
 modules.export = {
-    default: require('lib/IAMClient.js').default,
-    errorCode: require('lib/ErrorCodes.js').errorCode,
+    default: require('./lib/IAMClient.js').default,
+    errorCode: require('./lib/ErrorCodes.js').errorCode,
 };
