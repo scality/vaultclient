539b1ca7d81a47e36a199455c481cac8350299c2
Dora Korpar <dora.korpar@scality.com>
2016-12-02 11:31:57 -0800

    Add unit test ofr healthcheck

---
+++ tests/unit/healthcheckTest.js
@@ -0,0 +1,32 @@
+'use strict'; // eslint-disable-line
+
+const assert = require('assert');
+const http = require('http');
+const IAMClient = require('../../lib/IAMClient.js');
+
+function handler(req, res) {
+    res.writeHead(200);
+    return res.end();
+}
+
+describe('healthcheck test', () => {
+    let server;
+    let client;
+
+    beforeEach('start server', done => {
+        server = http.createServer(handler).listen(8500, () => {
+            client = new IAMClient('127.0.0.1', 8500);
+            done();
+        }).on('error', done);
+    });
+
+    afterEach('stop server', () => { server.close(); });
+
+    it('should return 200 on request', done => {
+        client.healthcheck(null, (err, obj, resCode) => {
+            assert.deepEqual(err, null);
+            assert.deepEqual(resCode, 200);
+            done();
+        });
+    });
+});
