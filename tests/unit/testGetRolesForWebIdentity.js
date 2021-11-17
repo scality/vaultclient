f50475365c83d099898de867996843c8a1d111db
williamlardier <william.lardier@scality.com>
2021-11-17 09:06:48 +0100

    improvement: VLTCLT-10 add missing getRolesForWebIdentity test

---
+++ tests/unit/testGetRolesForWebIdentity.js
@@ -0,0 +1,52 @@
+'use strict'; // eslint-disable-line
+
+const assert = require('assert');
+const http = require('http');
+
+const IAMClient = require('../../lib/IAMClient.js');
+
+describe('get-roles-for-web-identity', () => {
+    let server;
+    let client;
+
+    before('start server', done => {
+        server = http.createServer((req, res) => {
+            res.writeHead(200, { 'Content-Type': 'text/javascript' });
+            res.end('{}');
+        })
+            .on('error', done)
+            .listen(8500, () => {
+                done();
+            });
+        client = new IAMClient('127.0.0.1', 8500);
+    });
+
+    after('stop server', () => { server.close(); });
+
+    [
+        ['marker', -1, 'Marker must be a string'],
+        ['maxItems', '6', 'maxItems need to be a number'],
+        ['maxItems', parseInt('test', 10), 'maxItems must be a number'],
+        ['maxItems', 0,
+            'maxItems need to be a value between 1 and 1000 included'],
+        ['maxItems', 1500,
+            'maxItems need to be a value between 1 and 1000 included'],
+    ].forEach(test => {
+        // eslint-disable-next-line consistent-return
+        it(`invalid param ${test[0]}(${test[1]})`, next => {
+            const webIdentityToken = 'test';
+            try {
+                return client.getRolesForWebIdentity(webIdentityToken, {
+                    [test[0]]: test[1],
+                }, () => {
+                    assert(false,
+                        `Param ${test[0]} is invalid, must be caught`);
+                });
+            } catch (e) {
+                assert.deepStrictEqual(e.message,
+                    test[2]);
+                return next();
+            }
+        });
+    });
+});
