f0c3aea630bf1151d1443a7839129a0782714fe4
Mickael Bourgois <mickael.bourgois@scality.com>
2024-01-23 13:40:40 +0100

    VLTCLT-39: Update getAccounts function signature
    Use first arg for accountNames instead of
    overloading the function signature

--- tests/unit/getAccounts.js
+++ tests/unit/getAccounts.js
@@ -9,6 +9,7 @@ const canonicalIds = ['canId1', 'canId2'];
 const emailAddresses = ['email1', 'email2'];
 const accountNames = ['name1', 'name2'];
 const opt = { reqUid: 'test.getAccounts.reqUid' };
+const optAccountNames = Object.assign({}, opt, { accountNames: true });
 const mockCB = () => {};
 
 const expectedData = {
@@ -30,11 +31,12 @@ describe('getAccounts', () => {
 
     afterEach('reset spyArg', () => { spyArg = null; });
 
-    describe('should send request with regular function signature (5 args)', () => {
+    describe('should send request with correct arguments', () => {
         [
             { name: 'accountIds', args: [accountIds, null, null, opt, mockCB] },
             { name: 'emailAddresses', args: [null, emailAddresses, null, opt, mockCB] },
             { name: 'canonicalIds', args: [null, null, canonicalIds, opt, mockCB] },
+            { name: 'accountNames', args: [accountNames, null, null, optAccountNames, mockCB] },
         ].forEach(({ name, args }) => it(`for ${name}`, () => {
             client.getAccounts(...args);
             const [method, path, auth, cb, data, reqUid, contentType] = spyArg;
@@ -51,49 +53,9 @@ describe('getAccounts', () => {
         }));
     });
 
-    describe('should send request with new function signature (3args)', () => {
-        [
-            { name: 'accountIds', args: [{ accountIds }, opt, mockCB] },
-            { name: 'emailAddresses', args: [{ emailAddresses }, opt, mockCB] },
-            { name: 'canonicalIds', args: [{ canonicalIds }, opt, mockCB] },
-            { name: 'accountNames', args: [{ accountNames }, opt, mockCB] },
-        ].forEach(({ name, args }) => it(`for ${name}`, () => {
-            client.getAccounts(...args);
-            const [method, path, auth, cb, data, reqUid, contentType] = spyArg;
-            assert.strictEqual(method, 'GET');
-            assert.strictEqual(path, '/');
-            assert.strictEqual(auth, false);
-            assert.strictEqual(cb, mockCB);
-            assert.strictEqual(reqUid, opt.reqUid);
-            assert.strictEqual(contentType, null);
-            assert.deepStrictEqual(
-                data,
-                Object.assign({}, expectedData, { [name]: Object.values(args[0])[0] }),
-            );
-        }));
-    });
-
-    describe('should throw with regular function signature (5args)', () => {
+    describe('should throw with invalid arguments', () => {
         it('for all values set', () => {
             assert.throws(() => client.getAccounts(accountIds, emailAddresses, canonicalIds, opt, mockCB),
-                assert.AssertionError);
-        });
-    });
-
-    describe('should throw with new function signature (3args)', () => {
-        it('for all values set', () => {
-            assert.throws(() => client.getAccounts(
-                {
-                    accountIds, emailAddresses, canonicalIds, accountNames,
-                },
-                opt,
-                mockCB,
-            ),
-            assert.AssertionError);
-        });
-
-        it('for wrong argument type', () => {
-            assert.throws(() => client.getAccounts(accountIds, opt, mockCB),
                 assert.AssertionError);
         });
     });
