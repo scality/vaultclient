a0e95e9b0e2e28023b2886b3e433de970b098a97
Maha Benzekri <maha.benzekri@scality.com>
2024-03-19 09:41:13 +0100

    VLTCLT-42: Returned errors fixes
    This commit aims to return errors without relying on
    arsenal.

--- lib/constants.d.ts
+++ lib/constants.d.ts
@@ -2,5 +2,10 @@ export const httpClientFreeSocketTimeout: 55000;
 export namespace InternalError {
     const code: number;
     const description: string;
+    const InternalError: boolean;
+    namespace is {
+        const InternalError_1: boolean;
+        export { InternalError_1 as InternalError };
+    }
 }
 //# sourceMappingURL=constants.d.ts.map
