c9da2fd343b2396ad6350a96063fc6fa26d5d16b
Maha Benzekri <maha.benzekri@scality.com>
2024-03-24 21:40:29 +0100

    VLTCLT-42: removing is from error responses

--- lib/constants.d.ts
+++ lib/constants.d.ts
@@ -3,9 +3,5 @@ export namespace InternalError {
     const code: number;
     const description: string;
     const InternalError: boolean;
-    namespace is {
-        const InternalError_1: boolean;
-        export { InternalError_1 as InternalError };
-    }
 }
 //# sourceMappingURL=constants.d.ts.map
