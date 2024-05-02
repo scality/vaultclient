471a110f22420c484f0f921fc0c99f7e337b0941
Maha Benzekri <maha.benzekri@scality.com>
2024-05-02 12:06:51 +0200

    VLTCLT-42: fixing returned errors

--- lib/constants.d.ts
+++ lib/constants.d.ts
@@ -2,5 +2,6 @@ export const httpClientFreeSocketTimeout: 55000;
 export namespace InternalError {
     const code: number;
     const description: string;
+    const InternalError: boolean;
 }
 //# sourceMappingURL=constants.d.ts.map
