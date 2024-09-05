c1cb57a564ac58142a087a2639555b8d5a47b4d2
williamlardier <william.lardier@scality.com>
2024-09-05 14:51:27 +0200

    Regen types
    Issue: VLTCLT-47

--- lib/constants.d.ts
+++ lib/constants.d.ts
@@ -4,4 +4,25 @@ export namespace InternalError {
     let description: string;
     let InternalError: boolean;
 }
+export namespace InvalidParameterValue {
+    let code_1: number;
+    export { code_1 as code };
+    let description_1: string;
+    export { description_1 as description };
+    export let InvalidParameterValue: boolean;
+}
+export namespace Forbidden {
+    let code_2: number;
+    export { code_2 as code };
+    let description_2: string;
+    export { description_2 as description };
+    export let Forbidden: boolean;
+}
+export namespace WrongFormat {
+    let code_3: number;
+    export { code_3 as code };
+    let description_3: string;
+    export { description_3 as description };
+    export let WrongFormat: boolean;
+}
 //# sourceMappingURL=constants.d.ts.map
